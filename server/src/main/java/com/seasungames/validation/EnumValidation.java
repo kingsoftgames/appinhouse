d5a3c482c57ce46c2bc53a88342b77cc583b3af6
jianghaitao1221 <jianghaitao@kingsoft.com>
2020-04-26 19:09:49 +0800

    Add EnumValidation.

---
+++ server/src/main/java/com/seasungames/validation/EnumValidation.java
@@ -0,0 +1,47 @@
+package com.seasungames.validation;
+
+import com.seasungames.validation.EnumValidation.List;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by jianghaitao on 2020/4/26.
+ */
+@Documented
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {EnumValidator.class})
+@Repeatable(List.class)
+public @interface EnumValidation {
+
+    /**
+     * 枚举的class类型
+     */
+    Class<? extends java.lang.Enum<?>> enumClass();
+
+    String message() default "invalid Enum Value";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    /**
+     * 被调用的枚举属性的方法名，值用来判断是否合法
+     */
+    String method() default "ordinal";
+
+    @Documented
+    @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+    @Retention(RUNTIME)
+    @interface List {
+        EnumValidation[] value();
+    }
+}
---
+++ server/src/main/java/com/seasungames/validation/EnumValidator.java
@@ -0,0 +1,41 @@
+package com.seasungames.validation;
+
+import io.netty.util.internal.StringUtil;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.lang.reflect.Method;
+
+/**
+ * Created by jianghaitao on 2020/4/26.
+ */
+public class EnumValidator implements ConstraintValidator<EnumValidation, String> {
+
+    private EnumValidation annotation;
+
+    @Override
+    public void initialize(EnumValidation constraintAnnotation) {
+        annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if (StringUtil.isNullOrEmpty(value)) {
+            return false;
+        }
+        var enumClass = annotation.enumClass();
+        var enumValues = enumClass.getEnumConstants();
+        try {
+            Method method = enumClass.getMethod(annotation.method());
+            for (var o : enumValues) {
+                if (value.equals(method.invoke(o))) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+}

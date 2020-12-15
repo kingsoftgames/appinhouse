030d2cf95c5266aa7444fdca896d32fc0a5e82e4
姜海涛 <jianghaitao@kingsoft.com>
2020-12-15 14:04:22 +0800

    移除发布脚本，发布任务只负责更新docker image

--- deploy/copy-conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-APPINHOUSE_HOME=/srv/appinhouse
-REDIS_CONFIG_PATH=/srv/appinhouse/redis/conf
-APPINHOUSE_CONFIG_PATH=$APPINHOUSE_HOME/conf
-
-cp /tmp/appinhouse/docker-compose.yml $APPINHOUSE_HOME
-cp /tmp/appinhouse/redis.conf $REDIS_CONFIG_PATH
-cp /tmp/appinhouse/app.conf $APPINHOUSE_CONFIG_PATH
